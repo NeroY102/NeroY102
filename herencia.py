59f63428bf3db626195676cf4a7207d15b455999
NeroY102 <140418899+NeroY102@users.noreply.github.com>
2023-07-24 11:40:40 -0400

    Create herencia.py

---
+++ herencia.py
@@ -0,0 +1,13 @@
+class Vehiculos:
+  def __int__(self,Color,Rueda,Puerta):
+    self.Color = Color
+    self.Rueda = Rueda
+    self.Puerta = Puerta
+
+class Coches:
+  def __int__(self,Velocidad,Cilindrar,Motor):
+    self.Velocidad = Velocidad
+    self.Cilindrar = Cilindrar
+    self.Motor = Motor
+def Coches(self)
+print(Motor)
