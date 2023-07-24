e4a754623cb8b70aabd70f188d86f0638afb4aaf
NeroY102 <140418899+NeroY102@users.noreply.github.com>
2023-07-24 11:13:34 -0400

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,4 @@
-hm="Hola Mundo"
-print (hm)
+peso=float(input("Ingrese su Peso Kg:"))
+altura=float(input("Ingrese su Estatura m:"))
+IMC=round(peso/altura*2(altura,2),1)
+print("Su IMC es de"+str(IMC))
