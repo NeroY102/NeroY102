74d13aabbd7935fb2851dfb0560ae6082e64500a
NeroY102 <140418899+NeroY102@users.noreply.github.com>
2023-07-24 11:24:15 -0400

    Create año.py

---
+++ año.py
@@ -0,0 +1,8 @@
+Año=int(input("Ingrese Añon:"))
+if Año %4==0:
+  if Año %100=0:
+    if Año %400==0:
+    print("El Año es Bisiesto")
+    else ("El Año NO es Bisiesto")
+  else ("EL Año NO es Bisiesto")  
+else ("EL Año NO es Bisiesto")
